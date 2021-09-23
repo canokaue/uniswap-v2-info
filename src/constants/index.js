b44b006678863c1bafd38dfa68185618db1f1e37
top1st <riguang102@gmail.com>
2021-09-24 01:36:38 +0800

    update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xA3BceDADB23CfAA592aC42f5A18C98F98a37dA77'
 
 export const BUNDLE_ID = '1'
 
@@ -12,10 +12,7 @@ export const timeframeOptions = {
 }
 
 // token list urls to fetch tokens from - use for warnings on tokens and pairs
-export const SUPPORTED_LIST_URLS__NO_ENS = [
-  'https://gateway.ipfs.io/ipns/tokens.uniswap.org',
-  'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
-]
+export const SUPPORTED_LIST_URLS__NO_ENS = ['https://gateway.ipfs.io/ipns/tokens.uniswap.org']
 
 // hide from overview list
 export const TOKEN_BLACKLIST = [
